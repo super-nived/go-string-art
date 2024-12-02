589deed45c95725fbaabcf7011ee361d78702c8d
Ross Zurowski <ross@rosszurowski.com>
2024-12-02 13:57:19 -0500

    Use net/http (#1)
    This commit migrates this example to use Go's built-in net/http package,
    which is the standard way to build servers in Go. This new example just
    renders a simple static page, like the default express-hello-world
    example.
    
    This new version doesn't use a Dockerfile, since it demonstrates
    Render's native support for Go binaries, but I'm open to the argument
    that showing a Dockerfile may be more instructive in other ways.

--- README.md
+++ README.md
@@ -2,11 +2,4 @@
 
 This repo can be used as a starting point to deploy [Go](https://golang.org/) web applications on Render.
 
-It is a Dockerized version of the [realtime chat](https://github.com/gin-gonic/gin/tree/master/examples/realtime-advanced) example powered by the [Gin](https://github.com/gin-gonic/gin) web framework.
-
-The sample app is up at https://go-web.onrender.com.
-
-
-## Deployment
-
-See the guide at https://render.com/docs/deploy-go-web-server.
+It's a simple `net/http` Go webserver that embeds an HTML file and serves it.
--- main.go
+++ main.go
@@ -1,47 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
-	"runtime"
 
-	"github.com/gin-gonic/gin"
+	_ "embed"
 )
 
+//go:embed page.html
+var page string
+
 func main() {
-	ConfigRuntime()
-	StartWorkers()
-	StartGin()
-}
-
-// ConfigRuntime sets the number of operating system threads.
-func ConfigRuntime() {
-	nuCPU := runtime.NumCPU()
-	runtime.GOMAXPROCS(nuCPU)
-	fmt.Printf("Running with %d CPUs\n", nuCPU)
-}
-
-// StartWorkers start starsWorker by goroutine.
-func StartWorkers() {
-	go statsWorker()
-}
-
-// StartGin starts gin web server with setting router.
-func StartGin() {
-	gin.SetMode(gin.ReleaseMode)
-
-	router := gin.New()
-	router.Use(rateLimit, gin.Recovery())
-	router.LoadHTMLGlob("resources/*.templ.html")
-	router.Static("/static", "resources/static")
-	router.GET("/", index)
-	router.GET("/room/:roomid", roomGET)
-	router.POST("/room-post/:roomid", roomPOST)
-	router.GET("/stream/:roomid", streamRoom)
+	http.HandleFunc("/", serveHello)
 
 	port := os.Getenv("PORT")
-    if port == "" {
-    	port = "8080"
+	if port == "" {
+		port = "8080"
 	}
-	router.Run(":"+port)
+
+	log.Println("Server listening on http://localhost:" + port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatal("Error starting server:", err)
+	}
+}
+
+func serveHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, page)
 }
